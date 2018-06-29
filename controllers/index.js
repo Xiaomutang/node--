1966e7feba7596533aa85399a9a9d292863f3948
wangdong <1029507088@qq.com>
2018-06-29 21:44:03 +0800

    ....

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,17 @@
 // 导出
+const topicModel = require('../models/topic');
+const moment = require('moment');
 exports.showIndex = (req, res) => {
     // res.send("showIndex")
-    res.render('index.html');
+    // res.render('index.html');
+    topicModel.getAll((err, topics) => {
+        if (err) {
+            return res.send('服务器内部错误');
+        } 
+        res.render('index.html', {
+            user: req.session.user,
+            topics,
+            moment
+        });
+    })   
 }
---
+++ models/topic.js
@@ -0,0 +1,80 @@
+const db = require('./db_helper');
+exports.createTopic = (topic, callback) => {
+    db.query(
+        'insert into `topics` set ?',
+        topic,
+        (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            if (results.affectedRows > 0) {
+                callback(null, true);
+            } else {
+                callback(null, false);
+            }
+        }
+    )
+}
+
+exports.getById = (id, callback) => {
+    db.query(
+        'select * from `topics` where `id`=?',
+        id,
+        (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            if (results.length > 0) {
+                callback(null, results[0]);
+            } else {
+                callback(null, null);
+            }
+        }
+    )
+}
+
+exports.delete = (id, callback) => {
+    db.query(
+        'delete from `topic` where `id`=?',
+        id,
+        (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            if (results.affectedRows > 0) {
+                callback(null, true);
+            } else {
+                callback(null, false);
+            }
+        }
+    )
+}
+
+exports.update = (topic, callback) => {
+    db.query(
+        'update `topics` set `title`=?, `content`=?, categoryId=? where id=?',
+        [topic.title, topic.content, topic.categoryId, topic.id],
+        (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            if (results.affectedRows > 0) {
+                callback(null, true);
+            } else {
+                callback(null, false);
+            }
+        }
+    )
+}
+
+exports.getAll = (callback) => {
+    db.query(
+        'select topics.id, nickname, title, topics.createdAt from `topics` JOIN `users` ON userId = users.id',
+        (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, results);
+        }
+    )
+}
